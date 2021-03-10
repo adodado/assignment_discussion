ca7a18f1e537d8f9712f7875c543d43e647a891e
Admir Cosic <admir.cosic.82@gmail.com>
2021-03-10 17:42:33 +0100

    Delete ProductsPage.tsx

--- src/pages/ProductsPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ProductList from "../components/products/productList";
-
-const ProductsPage = () => {
-  return <ProductList />;
-};
-
-export default ProductsPage;

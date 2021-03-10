eb19ada23f1482f29eb31e7d992370dac9ef73a2
Admir Cosic <admir.cosic.82@gmail.com>
2021-03-10 17:42:07 +0100

    Delete CheckoutPage.tsx

--- src/pages/CheckoutPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import CheckoutItemList from "../components/cart/cartItemList";
-
-const CheckoutPage = () => {
-  return <CheckoutItemList />;
-};
-export default CheckoutPage;
